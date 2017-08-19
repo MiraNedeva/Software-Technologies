d7b5071bc8db0c277baa296e084525d7fb1256d8
MiraNedeva <mira.nedeva@yahoo.fr>
2017-08-19 12:20:34 +0200

    Upload of new application with the Spring Framework

---
+++ Java Blog/src/main/java/softuniBlog/bindingModel/ArticleBindingModel.java
@@ -0,0 +1,43 @@
+package softuniBlog.bindingModel;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuniBlog.entity.Article;
+
+import javax.jws.soap.SOAPBinding;
+
+
+import javax.validation.constraints.NotNull;
+
+
+
+public class ArticleBindingModel {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
